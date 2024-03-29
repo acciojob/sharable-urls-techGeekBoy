6ceb8b30a2f7323cfe79cbe9ee02a5fe64348a4b
techGeekBoy <148032840+techGeekBoy@users.noreply.github.com>
2024-03-29 15:03:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // your code here
+let form = getElementById("urlForm")
+let urlElement = getElementById("url")
+
+let value = form.addEventListener("submit",urlevent);
+  function urlevent(event){
+	  event.preventDefault();
+	  let name =  getElementById("name")
+	  let year =  getElementById("year")
+
+	  // const queryString = `?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+	  //   urlElement.textContent = `https://localhost:8080/${queryString}`;
+  }
